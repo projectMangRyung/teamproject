7134a59d2a7f24f538af77e9c33ccc97ee15bf58
younggi12 <oosc76@naver.com>
2026-06-29 17:14:53 +0900

    product: top버튼 추가

--- javascript/product.js
+++ javascript/product.js
@@ -121,4 +121,18 @@ document.querySelector(".btn_modal_buy").addEventListener("click", function() {
     window.location.href = "./cart.html";
 });
 
+// 탑버튼
+window.addEventListener("scroll", function() {
+    let topBtn = document.getElementById("topBtn");
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+});
+
+document.getElementById("topBtn").addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
 productload();
